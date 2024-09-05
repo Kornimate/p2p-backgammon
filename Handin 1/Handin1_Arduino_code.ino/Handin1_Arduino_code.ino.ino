f0dfd886ff8c49cc603bc643fdaad0b0c72569f8
LAPTOP-03D000GJ\mikke <202107199@post.au.dk>
2024-09-05 10:32:50 +0200

    Now works with arduino

---
+++ Handin 1/Handin1_Arduino_code.ino/Handin1_Arduino_code.ino.ino
@@ -0,0 +1,136 @@
+#include <Adafruit_SGP30.h>
+#include <M5StickCPlus.h>
+
+Adafruit_SGP30 sgp;
+
+// Define a struct to hold the CO2 and TVOC values
+struct AirQualityData {
+  uint16_t current_Co2;
+  uint16_t min_Co2;
+  uint16_t max_Co2;
+  uint16_t current_TVOC;
+  uint16_t min_TVOC;
+  uint16_t max_TVOC;
+};
+
+
+// Define an array to hold CO2 label values
+int label_Co2_title[] = {3,1};
+int label_Co2_now[] = {3,30};
+int label_Co2_min[] = {3,50};
+int label_Co2_max[] = {3,70};
+int label_TVOC_title[] = {3,120};
+int label_TVOC_now[] = {3,150};
+int label_TVOC_min[] = {3,170};
+int label_TVOC_max[] = {3,190};
+int label_num_meaure[] = {3,210};
+
+// Define the background color 
+uint16_t backgroundColor = BLACK;
+uint16_t textColor = WHITE; // Default text color (white)
+bool isWhiteBackground = false;   // Initially set to false (black background)
+
+
+// Define the start time
+int last_measurement = 0;
+int i = 0; 
+
+// Function to print the first element of the array to the M5StickC-Plus screen
+void write_to_screen(int* label, const char* msg, int text_size) {
+  int x_cord = label[0];
+  int y_cord = label[1];
+  M5.Lcd.setCursor(x_cord, y_cord); // Set cursor to the top-left corner of the screen
+  M5.Lcd.setTextSize(text_size);
+  M5.Lcd.setTextColor(textColor); // Set the text color
+  M5.Lcd.print(msg);
+
+}
+
+AirQualityData airData = {
+  0,
+  60000,
+  0,
+  0,
+  60000,
+  0
+};
+
+void measureAirQuality(){
+
+  if (sgp.IAQmeasure()) {
+    airData.current_Co2 = sgp.eCO2;
+    airData.current_TVOC = sgp.TVOC;
+
+    // Update the minimum and maximum eCO2 values
+    if (airData.current_Co2 < airData.min_Co2) airData.min_Co2 = airData.current_Co2;
+    if (airData.current_Co2 > airData.max_Co2) airData.max_Co2 = airData.current_Co2;
+    
+
+    // Update the minimum and maximum TVOC values
+    if (airData.current_TVOC < airData.min_TVOC) airData.min_TVOC = airData.current_TVOC;
+    if (airData.current_TVOC > airData.max_TVOC) airData.max_TVOC = airData.current_TVOC;
+
+  } else {
+    Serial.println("Measurement failed.");
+  }
+  last_measurement = millis();
+}
+
+
+void update_display(){
+  M5.Lcd.fillScreen(backgroundColor); // Fills the entire screen with black color
+  write_to_screen(label_Co2_title, "Co2", 3);
+  write_to_screen(label_TVOC_title, "TVOC", 3);
+
+  write_to_screen(label_Co2_now, ("Now: " + String(airData.current_Co2)).c_str(), 2);
+  write_to_screen(label_Co2_min, ("Min: " + String(airData.min_Co2)).c_str(), 2);
+  write_to_screen(label_Co2_max, ("Max: " + String(airData.max_Co2)).c_str(), 2);
+  write_to_screen(label_TVOC_now, ("Now: " + String(airData.current_TVOC)).c_str(), 2);
+  write_to_screen(label_TVOC_min, ("Min: " + String(airData.min_TVOC)).c_str(), 2);
+  write_to_screen(label_TVOC_max, ("Max: " + String(airData.max_TVOC)).c_str(), 2);
+  write_to_screen(label_num_meaure, ("#: " + String(i)).c_str(), 1);
+  
+}
+
+void measure_and_update(){
+  M5.Lcd.fillScreen(backgroundColor); // Clear the screen with black color
+  measureAirQuality();
+  update_display();
+}
+
+void setup() {
+  Serial.begin(38400);
+  // Initialize the M5StickC-Plus
+  M5.begin();
+  sgp.begin();
+  M5.Lcd.setRotation(0); // Set screen rotation if necessary
+  measure_and_update();
+}
+
+
+void loop() {
+  M5.update();  // Necessary to read button states properly
+
+  if (M5.BtnA.wasPressed()) {
+    // Toggle between white and black background
+    if (isWhiteBackground) {
+      backgroundColor = BLACK;
+      textColor = WHITE;
+    } else {
+      backgroundColor = WHITE;
+      textColor = BLACK;
+    }
+
+    isWhiteBackground = !isWhiteBackground; // Toggle the flag
+    update_display(); // Update the screen with the new colors
+  }
+
+  // Nothing to do in the loop for now
+  int current_time = millis();
+  if((current_time -last_measurement) >= 5000){
+    i = i+1;
+    measure_and_update();
+    last_measurement = current_time;
+  }
+  delay(100); // Small delay to prevent excessive button presses
+}
---
+++ Handin 1/co2_tvoc_program.py
@@ -0,0 +1,174 @@
+import os, sys, io
+import M5
+from M5 import *
+from hardware import *
+from unit import TVOCUnit
+import time
+
+
+
+co2_now = None
+label_tvoc = None
+co2_min = None
+co2_max = None
+tvoc_min = None
+tvoc_max = None
+Co2_title = None
+TVOC_title = None
+Last_update = None
+update_time = None
+i2c0 = None
+tvoc_0 = None
+
+
+import random
+
+cur_val = None
+cur_max = None
+hour = None
+tvoc_mesaurement = None
+current_time = None
+r_code = None
+max_tvoc = None
+co2_measurement = None
+g_code = None
+max_co2 = None
+min_tvoc = None
+last_update = None
+b_code = None
+min_co2 = None
+Background_color = None
+Text_color = None
+minute = None
+second = None
+
+# Describe this function...
+def update_max():
+  global cur_val, cur_max, hour, tvoc_mesaurement, current_time, r_code, max_tvoc, co2_measurement, g_code, max_co2, min_tvoc, last_update, b_code, min_co2, Background_color, Text_color, minute, second, co2_now, label_tvoc, co2_min, co2_max, tvoc_min, tvoc_max, Co2_title, TVOC_title, Last_update, update_time, i2c0, tvoc_0
+  if max_co2 == None:
+    max_tvoc = tvoc_mesaurement
+    min_tvoc = tvoc_mesaurement
+    max_co2 = co2_measurement
+    min_co2 = co2_measurement
+  else:
+    if check_for_new_max(tvoc_mesaurement, max_tvoc):
+      max_tvoc = tvoc_mesaurement
+    if check_for_new_max(co2_measurement, max_co2):
+      max_co2 = co2_measurement
+    if check_for_new_min(tvoc_mesaurement, min_tvoc):
+      min_tvoc = tvoc_mesaurement
+    if check_for_new_min(co2_measurement, min_co2):
+      min_co2 = co2_measurement
+
+# Describe this function...
+def Measure():
+  global cur_val, cur_max, hour, tvoc_mesaurement, current_time, r_code, max_tvoc, co2_measurement, g_code, max_co2, min_tvoc, last_update, b_code, min_co2, Background_color, Text_color, minute, second, co2_now, label_tvoc, co2_min, co2_max, tvoc_min, tvoc_max, Co2_title, TVOC_title, Last_update, update_time, i2c0, tvoc_0
+  tvoc_mesaurement = tvoc_0.tvoc()
+  co2_measurement = tvoc_0.co2eq()
+  update_max()
+  Update_display()
+
+# Describe this function...
+def Update_display():
+  global cur_val, cur_max, hour, tvoc_mesaurement, current_time, r_code, max_tvoc, co2_measurement, g_code, max_co2, min_tvoc, last_update, b_code, min_co2, Background_color, Text_color, minute, second, co2_now, label_tvoc, co2_min, co2_max, tvoc_min, tvoc_max, Co2_title, TVOC_title, Last_update, update_time, i2c0, tvoc_0
+  co2_now.setText(str((str('now: ') + str(co2_measurement))))
+  co2_min.setText(str((str('min: ') + str(min_co2))))
+  co2_max.setText(str((str('max: ') + str(max_co2))))
+  label_tvoc.setText(str((str('now: ') + str(tvoc_mesaurement))))
+  tvoc_min.setText(str((str('min: ') + str(min_tvoc))))
+  tvoc_max.setText(str((str('max: ') + str(max_tvoc))))
+  hour = (time.localtime())[3]
+  minute = (time.localtime())[4]
+  second = (time.localtime())[5]
+  update_time.setText(str((str(hour) + str(((str(':') + str(((str(minute) + str(((str(':') + str(second)))))))))))))
+
+# Describe this function...
+def check_for_new_max(cur_val, cur_max):
+  global hour, tvoc_mesaurement, current_time, r_code, max_tvoc, co2_measurement, g_code, max_co2, min_tvoc, last_update, b_code, min_co2, Background_color, Text_color, minute, second, co2_now, label_tvoc, co2_min, co2_max, tvoc_min, tvoc_max, Co2_title, TVOC_title, Last_update, update_time, i2c0, tvoc_0
+  if cur_val >= cur_max:
+    return True
+  return False
+
+# Describe this function...
+def update_every_min():
+  global cur_val, cur_max, hour, tvoc_mesaurement, current_time, r_code, max_tvoc, co2_measurement, g_code, max_co2, min_tvoc, last_update, b_code, min_co2, Background_color, Text_color, minute, second, co2_now, label_tvoc, co2_min, co2_max, tvoc_min, tvoc_max, Co2_title, TVOC_title, Last_update, update_time, i2c0, tvoc_0
+  current_time = time.ticks_ms()
+  if current_time >= last_update + 5000:
+    last_update = current_time
+    Measure()
+
+# Describe this function...
+def check_for_new_min(cur_val, cur_max):
+  global hour, tvoc_mesaurement, current_time, r_code, max_tvoc, co2_measurement, g_code, max_co2, min_tvoc, last_update, b_code, min_co2, Background_color, Text_color, minute, second, co2_now, label_tvoc, co2_min, co2_max, tvoc_min, tvoc_max, Co2_title, TVOC_title, Last_update, update_time, i2c0, tvoc_0
+  if cur_val <= cur_max:
+    return True
+  return False
+
+# Describe this function...
+def Change_color():
+  global cur_val, cur_max, hour, tvoc_mesaurement, current_time, r_code, max_tvoc, co2_measurement, g_code, max_co2, min_tvoc, last_update, b_code, min_co2, Background_color, Text_color, minute, second, co2_now, label_tvoc, co2_min, co2_max, tvoc_min, tvoc_max, Co2_title, TVOC_title, Last_update, update_time, i2c0, tvoc_0
+  r_code = random.randint(100, 255)
+  g_code = random.randint(100, 255)
+  b_code = random.randint(100, 255)
+  Background_color = (r_code << 16) | (g_code << 8) | b_code
+  Text_color = 0x000000
+  Widgets.fillScreen(Background_color)
+  Co2_title.setColor(Text_color, Background_color)
+  TVOC_title.setColor(Text_color, Background_color)
+  co2_now.setColor(Text_color, Background_color)
+  co2_min.setColor(Text_color, Background_color)
+  co2_max.setColor(Text_color, Background_color)
+  label_tvoc.setColor(Text_color, Background_color)
+  tvoc_min.setColor(Text_color, Background_color)
+  tvoc_max.setColor(Text_color, Background_color)
+  Last_update.setColor(Text_color, Background_color)
+  update_time.setColor(Text_color, Background_color)
+
+
+def btnA_wasClicked_event(state):
+  global co2_now, label_tvoc, co2_min, co2_max, tvoc_min, tvoc_max, Co2_title, TVOC_title, Last_update, update_time, i2c0, tvoc_0, hour, tvoc_mesaurement, current_time, r_code, max_tvoc, co2_measurement, g_code, max_co2, min_tvoc, cur_val, cur_max, last_update, b_code, min_co2, Background_color, Text_color, minute, second
+  Change_color()
+
+
+def setup():
+  global co2_now, label_tvoc, co2_min, co2_max, tvoc_min, tvoc_max, Co2_title, TVOC_title, Last_update, update_time, i2c0, tvoc_0, hour, tvoc_mesaurement, current_time, r_code, max_tvoc, co2_measurement, g_code, max_co2, min_tvoc, cur_val, cur_max, last_update, b_code, min_co2, Background_color, Text_color, minute, second
+
+  M5.begin()
+  co2_now = Widgets.Label("co2_now", 0, 19, 1.0, 0xffffff, 0x222222, Widgets.FONTS.DejaVu12)
+  label_tvoc = Widgets.Label("tvoc", 0, 126, 1.0, 0xffffff, 0x222222, Widgets.FONTS.DejaVu12)
+  co2_min = Widgets.Label("co2_min", 0, 34, 1.0, 0xffffff, 0x222222, Widgets.FONTS.DejaVu12)
+  co2_max = Widgets.Label("co2_max", -1, 49, 1.0, 0xffffff, 0x222222, Widgets.FONTS.DejaVu12)
+  tvoc_min = Widgets.Label("tvoc_min", 0, 139, 1.0, 0xffffff, 0x222222, Widgets.FONTS.DejaVu12)
+  tvoc_max = Widgets.Label("tvoc_max", -1, 154, 1.0, 0xffffff, 0x222222, Widgets.FONTS.DejaVu12)
+  Co2_title = Widgets.Label("Co2", 0, 0, 1.0, 0xffffff, 0x222222, Widgets.FONTS.DejaVu18)
+  TVOC_title = Widgets.Label("TVOC", 0, 105, 1.0, 0xffffff, 0x222222, Widgets.FONTS.DejaVu18)
+  Last_update = Widgets.Label("Last Update", 0, 180, 1.0, 0xffffff, 0x222222, Widgets.FONTS.DejaVu18)
+  update_time = Widgets.Label("label0", 0, 206, 1.0, 0xffffff, 0x222222, Widgets.FONTS.DejaVu12)
+
+  BtnA.setCallback(type=BtnA.CB_TYPE.WAS_CLICKED, cb=btnA_wasClicked_event)
+
+  i2c0 = I2C(0, scl=Pin(33), sda=Pin(32), freq=100000)
+  tvoc_0 = TVOCUnit(i2c0)
+  time.timezone('GMT-2')
+  last_update = time.ticks_ms()
+  Measure()
+  Change_color()
+
+
+def loop():
+  global co2_now, label_tvoc, co2_min, co2_max, tvoc_min, tvoc_max, Co2_title, TVOC_title, Last_update, update_time, i2c0, tvoc_0, hour, tvoc_mesaurement, current_time, r_code, max_tvoc, co2_measurement, g_code, max_co2, min_tvoc, cur_val, cur_max, last_update, b_code, min_co2, Background_color, Text_color, minute, second
+  M5.update()
+  update_every_min()
+
+
+if __name__ == '__main__':
+  try:
+    setup()
+    while True:
+      loop()
+  except (Exception, KeyboardInterrupt) as e:
+    try:
+      from utility import print_error_msg
+      print_error_msg(e)
+    except ImportError:
+      print("please update to latest firmware")
