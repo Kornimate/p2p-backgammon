d0b05e5c805f6a1d6b70bc5a05a1b52996717cf7
Mate Kornidesz <Mate Kornidesz@gmail.com>
2024-09-10 13:31:00 +0200

    template for query created

--- Handin 2/query.js
+++ Handin 2/query.js
@@ -1,5 +1,51 @@
 window.onload = Initialization;
 
-function Initialization(){
-    //code comes here
+async function Initialization(){
+
+    await QueryFunction();
+
+    setInterval(QueryFunction, 10000); //update every 10 sec
+}
+
+async function QueryFunction(){
+
+    const queryTime = document.getElementById("lastQueryTime");
+
+    queryTime.innerHTML = "Loading...";
+
+    try{
+        
+        const resAwaitbale = await fetch("https://ezdata2.m5stack.com/api/v2/<owndata>");
+    
+        const jsonData = await resAwaitbale.json();
+            
+        PrintTableData(true, jsonData);
+        
+    }
+    catch{
+        PrintTableData(false, null);
+    }
+
+    queryTime.innerHTML = `${new Date()}`;
+}
+
+function PrintTableData(successfulQuery = false, jsonData){
+    
+    const table = document.getElementById("tableBody");
+
+    if(!successfulQuery){
+        table.innerHTML = '<tr><td colspan="3" class="error-text">Failed to fetch data</td></tr>';
+
+        return;
+    }
+
+    let content = "";
+
+    for(const key in jsonData){
+        //code comes here
+
+        content += `<tr><td>${}</td><td>${}</td><td>${}</td></tr>`;
+    }
+
+    table.innerHTML = content;
 }
