3a28c1179d6dfdc8cdc2626de56ea5ad6daf327d
Kornimate <kornimate@gmail.com>
2024-11-28 21:33:37 +0100

    refactored code

--- Project/react-client/src/components/GameBoardPieceHolder.js
+++ Project/react-client/src/components/GameBoardPieceHolder.js
@@ -1,27 +1,29 @@
 import blackImage from '../assets/black_player.png';
 import whiteImage from '../assets/white_player.png';
-import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 
 const GameBoardPieceHolder = ({x, size, piecesNum, isBlack, onTop}) => {
 
-    const [pieces, setPieces] = useState([]);
-
-    useEffect(() => {
-        setPieces([...Array(piecesNum).keys()])
-    }, [piecesNum])
+    const pieces = useMemo(() => [...Array(piecesNum).keys()], [piecesNum]);
 
     return (
         <>
         {
             pieces.map((element) => (
-                piecesNum > 0 && onTop && <img alt="" src={isBlack ? blackImage : whiteImage} style={{position: 'absolute', left: x, top: `${element*(size/3)+5}px`, width: size}} key={`${x}_${element}`} />
+                <img
+                    alt=""
+                    src={isBlack ? blackImage : whiteImage}
+                    style={{
+                        position: 'absolute',
+                        left: x,
+                        top: onTop ? `${element*(size/3)+5}px` : undefined,
+                        bottom: !onTop ? `${element*(size/3)+5}px` : undefined,
+                        width: size
+                    }}
+                    key={`${x}_${element}`} />
             ))
         }
-        {
-            pieces.map((element) => (
-               piecesNum > 0 && !onTop && <img alt="" src={isBlack ? blackImage : whiteImage} style={{position: 'absolute', left: x, bottom: `${element*(size/3)+5}px`, width: size}} key={`${x}_${element}`} />
-            ))
-        }
+
         </>
     )
 }
